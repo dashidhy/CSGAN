230a7d82fa5c037d5b2b5352c83780e873850152
Hongyuan Du <du-hongyuan@outlook.com>
2019-02-16 18:39:14 -0800

    parameter for pretrained nets

--- utils/style_utils.py
+++ utils/style_utils.py
@@ -10,29 +10,29 @@ import numpy as np
 
 from .gan_utils import deprocess_img
 
-SQUEEZENET_MEAN = np.array([0.485, 0.456, 0.406], dtype=np.float32)
-SQUEEZENET_STD = np.array([0.229, 0.224, 0.225], dtype=np.float32)
+PRE_MEAN = np.array([0.485, 0.456, 0.406], dtype=np.float32)
+PRE_STD = np.array([0.229, 0.224, 0.225], dtype=np.float32)
 
 def style_target_preprocess(style_target, size=512, batchsize=128):
     transform = T.Compose([
         T.Resize(size),
         T.ToTensor(),
-        T.Normalize(mean=SQUEEZENET_MEAN.tolist(),
-                    std=SQUEEZENET_STD.tolist()),
+        T.Normalize(mean=PRE_MEAN.tolist(),
+                    std=PRE_STD.tolist()),
         T.Lambda(lambda x: x.repeat(batchsize, 1, 1, 1)),
     ])
     return transform(style_target)
 
 def fake_image_preprocess(fake_image):
-    mean = torch.tensor(SQUEEZENET_MEAN.reshape((1, 3, 1, 1))).type(fake_image.type())
-    std = torch.tensor(SQUEEZENET_STD.reshape((1, 3, 1, 1))).type(fake_image.type())
+    mean = torch.tensor(PRE_MEAN.reshape((1, 3, 1, 1))).type(fake_image.type())
+    std = torch.tensor(PRE_STD.reshape((1, 3, 1, 1))).type(fake_image.type())
     return (deprocess_img(fake_image) - mean) / std
 
 def deprocess(img):
     transform = T.Compose([
         T.Lambda(lambda x: x[0]),
-        T.Normalize(mean=[0, 0, 0], std=[1.0 / s for s in SQUEEZENET_STD.tolist()]),
-        T.Normalize(mean=[-m for m in SQUEEZENET_MEAN.tolist()], std=[1, 1, 1]),
+        T.Normalize(mean=[0, 0, 0], std=[1.0 / s for s in PRE_STD.tolist()]),
+        T.Normalize(mean=[-m for m in PRE_MEAN.tolist()], std=[1, 1, 1]),
         T.Lambda(rescale),
         T.ToPILImage(),
     ])
